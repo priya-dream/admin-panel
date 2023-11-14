e6c4519bcfda97399922a2b15af0023d4cf7bfe5
priya-dream <n.priya199720@gmail.com>
2023-11-14 19:58:08 +0530

    changes

---
+++ tests/Unit/CompanyTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class CompanyTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        //$this->assertTrue(true);
+        //$this->assertTrue('this is a testing');
+        $name = "Priya";
+        $this->assertTrue($name == "mathu");
+    }
+}
